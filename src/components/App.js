8009a2b5e079e5ef252aa257bdc622864c03dbbf
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-06-30 09:55:03 -0700

    nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Nav from './Nav';
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+        <Nav />
+    </div>
   )
 }
