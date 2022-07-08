4c97500be41634bc9384aac082d3ab09d6ffbe07
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-07-08 10:00:44 -0700

    little styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import '../style/About.css';
 
 export default function About() {
     return (
-        <div>
-            <h2>
-                Samantha has extensive local knowledge from being raised on the Gulf Islands and going to the University of Victoria. She graduated with a Bachelor of Fine Arts holding the title of distinction and honours, she specialized in photography and sculpture. Her post-secondary education has taught her the ability to be creative in the real estate business. Samantha’s unique background allows her to confidently describe homes from a community perspective. Samantha prides herself in her drive to work hard in order to be successful. Believing that being a genuine and honest person will allow her to be the best. Samantha is excited to be working alongside a real estate brokerage that has established an incredible name internationally. Working with Sotheby’s International Realty Canada means giving a luxury experience to every client. She is dedicated to ensuring that whether you are selling or buying a home you will receive the utmost due diligence from her service.
-
+        <div className='about-p'>
+            <h2>About Samantha</h2>
+            <h4>
+                Samantha has extensive local knowledge from being raised on the Gulf Islands and going to the University of Victoria. She graduated with a Bachelor of Fine Arts holding the title of distinction and honours, she specialized in photography and sculpture. Her post-secondary education has taught her the ability to be creative in the real estate business. Samantha’s unique background allows her to confidently describe homes from a community perspective. <br/> Samantha prides herself in her drive to work hard in order to be successful. Believing that being a genuine and honest person will allow her to be the best. Samantha is excited to be working alongside a real estate brokerage that has established an incredible name internationally. Working with Sotheby’s International Realty Canada means giving a luxury experience to every client. She is dedicated to ensuring that whether you are selling or buying a home you will receive the utmost due diligence from her service.
+                <br/>
                 With Samantha’s local expertise she will be more than happy to find you the perfect area to suit your lifestyle whether you want to be in the heart of downtown or secluded in the temperate rainforest. She knows of excellent dining spots, outstanding hiking trails, and local activities throughout the Islands. Samantha considers Victoria to be her home and she is so very excited to introduce you to the cities hidden gems.
-
-                Service Areas
+                <br/>
+                Service Areas: <br/>
                 Gulf Islands, Vancouver Island, Sooke (Greater Victoria), Victoria (Greater Victoria)
-
-                Languages
-                English
-            </h2>
+                <br/>
+            </h4>
         </div>
     )
 }
